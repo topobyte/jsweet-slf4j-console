6200fa592e10c124cad9cb9bf2241f74bc631d10
Sebastian Kürten <sebastian@topobyte.de>
2017-11-08 11:06:26 +0100

    Fix typo: slfj4 -> slf4j

--- src/main/java/org/slf4j/LogLevel.java
+++ src/main/java/org/slf4j/LogLevel.java
@@ -1,19 +1,19 @@
 // Copyright 2017 Sebastian Kuerten
 //
-// This file is part of jsweet-slfj4-console.
+// This file is part of jsweet-slf4j-console.
 //
-// jsweet-slfj4-console is free software: you can redistribute it and/or modify
+// jsweet-slf4j-console is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// jsweet-slfj4-console is distributed in the hope that it will be useful,
+// jsweet-slf4j-console is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with jsweet-slfj4-console. If not, see <http://www.gnu.org/licenses/>.
+// along with jsweet-slf4j-console. If not, see <http://www.gnu.org/licenses/>.
 
 package org.slf4j;
 
